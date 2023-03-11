a95111005980002080b9c52a1cbc53a295e147cb
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-03-10 16:58:06 -0800

    name change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='Stanford Extract',
+setup(name='STARR-Labeler',
 version='0.0.1',
 author='Louis Blankemeier',
 description='Transform raw electronic health records data into features and labels.',
