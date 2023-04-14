d867ef165f8460c00d9341494c0a8c1aee0b07f9
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-04-13 21:37:30 -0700

    working without truncate

--- starr_labeler/features/ehr_types/demographics/extract_demographics.py
+++ starr_labeler/features/ehr_types/demographics/extract_demographics.py
@@ -12,10 +12,14 @@ class extract_demographics(extract_base):
         demographics_regex = "|".join(list(self.cfg['FEATURES']['TYPES']['DEMOGRAPHICS']['INCLUDE'].keys()))
         demographics_regex += '|Patient Id'
         pat_data = pat_data.loc[:, list(pd.Series(pat_data.columns).str.contains(demographics_regex, regex = True, case = False, na = False))]
-        pat_data.loc[:, 'Date of Birth'] = pd.to_datetime(pat_data.loc[:, 'Date of Birth'], utc=True)
-        pat_data.loc[:, 'Race'] = pat_data.loc[:, 'Race'].astype('category').cat.codes
-        pat_data.loc[:, 'Gender'] = pat_data.loc[:, 'Gender'].astype('category').cat.codes
-        pat_data.loc[:, 'Ethnicity'] = pat_data.loc[:, 'Ethnicity'].astype('category').cat.codes
+        if 'Date of Birth' in pat_data.columns:
+            pat_data.loc[:, 'Date of Birth'] = pd.to_datetime(pat_data.loc[:, 'Date of Birth'], utc=True)
+        if 'Race' in pat_data.columns:
+            pat_data.loc[:, 'Race'] = pat_data.loc[:, 'Race'].astype('category').cat.codes
+        if 'Gender' in pat_data.columns:
+            pat_data.loc[:, 'Gender'] = pat_data.loc[:, 'Gender'].astype('category').cat.codes
+        if 'Ethnicity' in pat_data.columns:
+            pat_data.loc[:, 'Ethnicity'] = pat_data.loc[:, 'Ethnicity'].astype('category').cat.codes
         return pat_data
 
     def truncate_data(self, pat_data):
