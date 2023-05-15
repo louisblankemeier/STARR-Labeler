b7dafa9f99ddbad9bccc65e2927c94edaa9e02c0
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-05-15 13:43:35 -0700

    reformatted

--- starr_labeler/features/ehr_types/labs/investigate_labs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from starr_labeler.utils import data_iterator, frequency_by_num_patients, get_parser, setup_cfg
-
-if __name__ == "__main__":
-    args = get_parser().parse_args()
-    cfg = setup_cfg(args)
-    iterator = data_iterator(os.path.join(cfg.FEATURES.PATH, "clinical_data"), "labs.csv")
-    frequency_by_num_patients(iterator, "Lab", "./lab_broader_frequencies_by_patient.png", 50, 20)
