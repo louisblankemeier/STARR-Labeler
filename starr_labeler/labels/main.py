bc458a256ae8b0ac082936d80f251ac780173cd6
Louis Blankemeir <louis.blankemeier@stanford.edu>
2023-05-15 10:03:45 -0700

    delete config path from main

--- starr_labeler/labels/main.py
+++ starr_labeler/labels/main.py
@@ -7,7 +7,7 @@ from typing import List, Dict, Tuple, NamedTuple, Optional
 
 from starr_labeler.labels.label_generator import label_generator
 
-@hydra.main(version_base=None, config_path="disease_configs/", config_name = 'aaa.yaml')
+@hydra.main(version_base=None, config_path="disease_configs/")
 def main(cfg):
     labels_class = label_generator(cfg)
     labels_class.compute_diagnosis_dates()
