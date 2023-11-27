b423f5ce44669b5b0dabef85bcb9008c8757f31f
Louis Blankemeier <louis.blankemeier@stanford.edu>
2023-11-27 15:21:05 -0800

    heart disease configs

--- starr_labeler/labels/python_submit.py
+++ starr_labeler/labels/python_submit.py
@@ -9,12 +9,12 @@ def python_submit(command, node=None, ngpus=0):
     bash_file.close()
     if node is None:
         os.system(
-            f"sbatch --ntasks=8 --ntasks-per-node=8 --gres=gpu:{ngpus}"
-            f" --output ./slurm/slurm-%j.out --mem=256000 --time=100-00:00:00 slurm.sh "
+            f"sbatch --ntasks=4 --ntasks-per-node=4 --gres=gpu:{ngpus}"
+            f" --output ./slurm/slurm-%j.out --time=100-00:00:00 slurm.sh "
         )
     else:
         os.system(
-            f"sbatch --ntasks=8 --ntasks-per-node=8 --gres=gpu:{ngpus}"
-            f" --output ./slurm/slurm-%j.out --mem=256000 --nodelist={node} --time=100-00:00:00 slurm.sh"
+            f"sbatch --ntasks=4 --ntasks-per-node=4 --gres=gpu:{ngpus}"
+            f" --output ./slurm/slurm-%j.out --nodelist={node} --time=100-00:00:00 slurm.sh"
         )
     os.remove("./slurm.sh")
--- starr_labeler/labels/slurm.py
+++ starr_labeler/labels/slurm.py
@@ -1,3 +1,9 @@
 from python_submit import python_submit
 
-python_submit("python main.py --config-name fragility_fracture.yaml", node="siena")
+python_submit("python main.py --config-name afib.yaml", node="roma")
+
+python_submit("python main.py --config-name heart_failure.yaml", node="roma")
+
+python_submit("python main.py --config-name cardiovascular_disease.yaml", node="roma")
+
+python_submit("python main.py --config-name cardiovascular_disease_afib_heart_failure.yaml", node="roma")
