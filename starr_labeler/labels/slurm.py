70fd110538116a52162e72e3390c01e0be3c01c5
Louis Blankemeir <lblankem@roma.stanford.edu>
2024-01-18 16:21:11 -0800

    disease configs

--- starr_labeler/labels/slurm.py
+++ starr_labeler/labels/slurm.py
@@ -1,9 +1,16 @@
 from python_submit import python_submit
 
-python_submit("python main.py --config-name afib.yaml", node="roma")
+# python_submit("python main.py --config-name afib.yaml", node="roma")
 
-python_submit("python main.py --config-name heart_failure.yaml", node="roma")
+# python_submit("python main.py --config-name heart_failure.yaml", node="roma")
+
+# python_submit("python main.py --config-name cardiovascular_disease.yaml", node="roma")
+
+# python_submit("python main.py --config-name cardiovascular_disease_afib_heart_failure.yaml", node="roma")
 
 python_submit("python main.py --config-name cardiovascular_disease.yaml", node="roma")
-
-python_submit("python main.py --config-name cardiovascular_disease_afib_heart_failure.yaml", node="roma")
+python_submit("python main.py --config-name chronic_kidney_disease.yaml", node="roma")
+python_submit("python main.py --config-name diabetes.yaml", node="roma")
+python_submit("python main.py --config-name hypertension.yaml", node="roma")
+python_submit("python main.py --config-name ischemic_heart_disease.yaml", node="roma")
+python_submit("python main.py --config-name osteoporosis.yaml", node="roma")
